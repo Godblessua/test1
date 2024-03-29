d79f6c02602f303645a8851c3a18330c32446304
yzh <dekuyu@163.com>
2024-03-29 17:07:12 +0800

    删除

--- remove-watermark-uniapp-frontend/src/pages/more/more.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Profile />
-</template>
-
-<script lang="ts" setup>
-import Profile from './profile/Profile.vue'
-</script>
-
-<style scoped></style>
--- remove-watermark-uniapp-frontend/src/types/component.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/components.d.ts
-//声明组件类型
-import RwSwiper from '@/components/RwSwiper.vue'
-declare module 'vue' {
-  export interface GlobalComponents {
-    RwSwiper: typeof RwSwiper
-  }
-}
